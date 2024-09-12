f8a3e9c866833a092c75c34f1845b795d80e5ac5
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-09-12 09:09:40 -0300

    Alterações Home

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -20,50 +20,62 @@ class _HomeViewState extends State<HomeView> {
         title: Text('Calcular Gorgeja'),
         titleTextStyle: TextStyle(),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Form(
-          key: formKey,
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: txtVlrConta,
-                  decoration: InputDecoration(
-                    hintText: 'Valor da Conta',
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: txtVlrConta,
+                decoration: InputDecoration(
+                  hintText: 'Valor da Conta',
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                controller: txtPGorjeta,
+                decoration: InputDecoration(
+                  hintText: 'Porcentagem da gorjeta',
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SwitchListTile(
+                title: Text('Arredondar?'),
+                value: arredondar,
+                onChanged: (bool value) {
+                  setState(() {
+                    arredondar = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(30, 45),
+                        textStyle: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        // calculo gorjeta aqui
+                        if (arredondar == false) {
+                          // arredondar aqui
+                        }
+                      },
+                      child: Text('calcular'),
+                    ),
                   ),
-                ),
-                TextFormField(
-                  controller: txtPGorjeta,
-                  decoration: InputDecoration(
-                    hintText: 'Porcentagem da gorjeta',
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(50, 40),
-                    textStyle: TextStyle(fontSize: 15),
-                  ),
-                  onPressed: () {
-                    // calculo gorjeta aqui
-                    if (arredondar == false) {
-                      // arredondar aqui
-                    }
-                  },
-                  child: Text('calcular'),
-                ),
-                SwitchListTile(
-                  title: Text('Arredondar?'),
-                  value: arredondar,
-                  onChanged: (bool value) {
-                    setState(() {
-                      arredondar = value;
-                    });
-                  },
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
